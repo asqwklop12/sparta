b67db2a46fd9ca3fad25dac859647c6ffb26efcf
yonghun <asqwklop12@gmail.com>
2025-02-06 00:29:47 +0900

    aop 적용

--- src/main/java/com/sparta/myselectshop/repository/ApiUseTimeRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ApiUseTimeRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ApiUseTimeRepository extends JpaRepository<ApiUseTime, Long> {
+public interface  ApiUseTimeRepository extends JpaRepository<ApiUseTime, Long> {
     Optional<ApiUseTime> findByUser(User user);
 }
